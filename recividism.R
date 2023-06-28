6012f54bae61ca9a331a213bcc5ab5d8190cc6c4
Ricardo J. Serrano <rserran1@gmail.com>
2023-06-27 20:32:39 -0400

    Add British Journal of Cancer Survival Analysis Part i - IV articles, add Rossi prisoner recividism time-to-event dataset and Cox PH regression model and diagnostics

---
+++ recividism.R
@@ -0,0 +1,42 @@
+# An illustration: Recividism
+# Source: Appendix-Cox-Regression,pdf (3.2)
+
+# load packages
+library(carData)
+library(survival)
+
+# laod Rossi dataset
+data("Rossi")
+head(Rossi[, 1:10])
+
+unique(Rossi$week)
+
+# Cox PH regression model
+mod.allison <- coxph(Surv(week, arrest) ~ fin + age + race + wexp + 
+                          mar + paro + prio, 
+                     data = Rossi)
+
+mod.allison
+
+summary(mod.allison)
+
+# ANOVA
+library(car)
+Anova(mod.allison)
+
+# graphical test of PH
+library(survminer)
+
+ggcoxzph(cox.zph(mod.allison))
+
+# forest plot
+ggforest(mod.allison)
+
+# Cox PH diagnostics
+ggcoxdiagnostics(mod.allison, type = 'deviance')
+
+ggcoxdiagnostics(mod.allison, type = 'martingale')
+
+# heart failure dataset
+data("heart")
+head(jasa)
